cb49fde4e4e66c0168bf6774012945d4bbb0d439
Celeste Cruz <celesteglezcruz@gmail.com>
2026-03-06 13:59:52 -0500

    chore: susupendiendo frima crypto

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -9,22 +9,22 @@ export const api = axios.create({
   },
 });
 
-api.interceptors.request.use((config) => {
-  const timestamp = Date.now().toString();
-  const secret = process.env.NEXT_PUBLIC_API_SECRET || "default_secret";
+// api.interceptors.request.use((config) => {
+//   const timestamp = Date.now().toString();
+//   const secret = process.env.NEXT_PUBLIC_API_SECRET || "default_secret";
 
-  // Construir el payload a firmar: MÉTODO + RUTA + TIMESTAMP + DATA(si existe)
-  // Ejemplo: POST/users1765876543{"nombre":"..."}
-  const method = config.method?.toUpperCase() || "";
-  const url = config.url || "";
-  const dataString = config.data ? JSON.stringify(config.data) : "";
+//   // Construir el payload a firmar: MÉTODO + RUTA + TIMESTAMP + DATA(si existe)
+//   // Ejemplo: POST/users1765876543{"nombre":"..."}
+//   const method = config.method?.toUpperCase() || "";
+//   const url = config.url || "";
+//   const dataString = config.data ? JSON.stringify(config.data) : "";
 
-  const payloadToSign = `${method}${url}${timestamp}${dataString}`;
+//   const payloadToSign = `${method}${url}${timestamp}${dataString}`;
 
-  const signature = CryptoJS.HmacSHA256(payloadToSign, secret).toString();
+//   const signature = CryptoJS.HmacSHA256(payloadToSign, secret).toString();
 
-  config.headers["x-timestamp"] = timestamp;
-  config.headers["x-signature"] = signature;
+//   config.headers["x-timestamp"] = timestamp;
+//   config.headers["x-signature"] = signature;
 
-  return config;
-});
+//   return config;
+// });
